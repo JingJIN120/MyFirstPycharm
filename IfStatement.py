ead53975fd47cbe85a9e879fb49d78714a140330
jinjing <jing.jin@my.jcu.edu.au>
2019-03-27 19:34:01 +0800

    IfSstatement

---
+++ IfStatement.py
@@ -0,0 +1,26 @@
+
+# If 条件语句
+# else
+# elif
+
+
+a=20
+b=40
+
+if a < b:
+    print("good morning")
+if b<50:
+    print("good afternoon")
+
+age=int(input("please type your age: "))
+if age<18:
+    print("You can not drink alcohol")
+else:
+    print("You can drink alcohol")
+elif age==18
+    print("You are 18 years old now, you can go to drink")
+elif age==100
+    print("You are 100 years old now, please quit drink better for your health")
+
+
+
