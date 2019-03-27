ec702da8c7ac9435b599b35624347e6a9522c391
jinjing <jing.jin@my.jcu.edu.au>
2019-03-27 19:33:22 +0800

    Function

---
+++ Function.py
@@ -0,0 +1,31 @@
+
+
+# function 函数
+# def=define 下定义
+
+def func1():
+    print("hello function1")
+
+
+a=10
+b=10
+print(a+b)
+
+c=20
+d=20
+print(a+b)
+
+def sum(num1, num2):
+    print(num1+num2)
+
+sum(1,2)
+sum(10,10)
+
+def func2():
+    return "hello function2"
+
+b=sum2(1,1)
+print(b)
+
+
+
